5020e6424f14324907b2d8a517daa76cd7a90f80
Cevdet Eren <82716289+cevdetozbozkurt@users.noreply.github.com>
2022-12-11 23:10:12 +0300

    Tablelara bilgi girildi altkategori kategoriye baglanacak

---
+++ updateTables.sql
@@ -0,0 +1,27 @@
+﻿select AltKategoriadi from AltKategori 
+inner join kategori on altkategori.kategoriid = kategori.kategoriid 
+
+select * from AltKategori
+
+
+insert into AltKategori(AltKategoriAdi,KategoriId) values('Oyun & Oyun Konsolları',1)
+
+update AltKategori
+set AltKategoriAdi = 'Klima ve Isıtıcılar'
+where AltKategoriId = 7
+
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
+
+ALTER TABLE siparis
+drop COLUMN uyeid,adresid;
+
+alter table siparis
+add UyeId int foreign key(Uyeid) references Uye(UyeId), 
+AdresId int foreign key (AdresId) references UyeAdres(AdresId)
