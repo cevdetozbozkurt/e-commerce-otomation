d9fe7dc1469ebe1f04bb217c3fd89838fa2c9523
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-12-24 02:42:48 +0300

    5 adet form oluşturuldu

---
+++ e-commere/SelinForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_commere
+{
+    public partial class SelinForm2 : Form
+    {
+        public SelinForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
