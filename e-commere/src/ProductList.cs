1ddbdccda2173ec894f1b31c5f2064236fc173c3
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-12-25 20:42:37 +0300

    otomatik picturebox oluşturma

--- e-commere/src/ProductList.cs
+++ e-commere/src/ProductList.cs
@@ -16,28 +16,35 @@ namespace e_commere.src
         public static void CreatePanel(int numberOfItem, Form form)
         {
             List<Panel> panel = new List<Panel>();
+            List<PictureBox> pictureBoxes = new List<PictureBox>();
             int witdh = 118;
             int height = 0;
             for (int i = 0; i < numberOfItem; i++,height++)
             {
-                if((i+1)%4 == 0)
+                if (height == 4)
                 {
-                    witdh = 305;
                     height = 0;
                 }
-                Debug.WriteLine(height);
                 
-                //(numberOfItem % 4 != 0 ? 182 : 470)
                 panel.Add(new Panel());
                 panel[i].BorderStyle = BorderStyle.FixedSingle;
                 panel[i].Location= new Point(199+(height*139), witdh);
                 panel[i].Size = new Size(133,175);
-
                 panel[i].Margin = new Padding(3,2,3,2);
                 panel[i].Anchor = AnchorStyles.None;
                 panel[i].TabIndex = 70;
                 form.Controls.Add(panel[i]);
-                
+                pictureBoxes.Add(new PictureBox());
+                panel[i].Controls.Add(pictureBoxes[i]);
+                pictureBoxes[i].Location = new Point(3, 2);
+                pictureBoxes[i].Size = new Size(125, 117);
+                pictureBoxes[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBoxes[i].Image = null;
+                if ((i + 1) % 4 == 0)
+                {
+                    witdh = 305;
+                    
+                }
             }
         }
     }
